d7a80a3601af71e89e39aa7479c0ad42abf7087a
Benjamin Sutherland <blender.sleuth@gmail.com>
2016-05-15 17:57:00 +1000

    Added UI Elements

---
+++ Circles/CirclesNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  CirclesNavigationController.swift
+//  Circles
+//
+//  Created by Ben Sutherland on 15/05/2016.
+//  Copyright © 2016 blendersleuthgraphics. All rights reserved.
+//
+
+import UIKit
+
+class CirclesNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Circles/GameViewController.swift
+++ Circles/GameViewController.swift
@@ -15,15 +15,10 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
 
         if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
@@ -33,20 +28,6 @@ class GameViewController: UIViewController {
     override func shouldAutorotate() -> Bool {
         return true
     }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
