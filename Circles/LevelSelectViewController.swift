8fa3bb0ca30619b6ff015def88d216b0c2bde0ff
Benjamin Sutherland <blender.sleuth@gmail.com>
2016-05-17 16:27:08 +1000

    Added icon and basic UI Elements

--- Circles/LevelSelectViewController.swift
+++ Circles/LevelSelectViewController.swift
@@ -12,24 +12,15 @@ class LevelSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playButtonPressed(sender: AnyObject) {
+        if let WinViewController = storyboard?.instantiateViewControllerWithIdentifier("WinViewController") as? WinViewController {
+            navigationController?.pushViewController(WinViewController, animated: true)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
     }
-    */
 
 }
--- Circles/MainMenuViewController.swift
+++ Circles/MainMenuViewController.swift
@@ -10,26 +10,14 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playButtonPressed(sender: AnyObject) {
+        if let levelSelectViewController = storyboard?.instantiateViewControllerWithIdentifier("LevelSelectViewController") as? LevelSelectViewController {
+            navigationController?.pushViewController(levelSelectViewController, animated: true)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
